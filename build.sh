012dc847c28a4bd21c23fc9d66e1f537362dbad6
Heiko Ziegler <git@z5r.de>
2016-03-14 01:05:39 +0100

    parameters

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ VERSION=2.6.0
 
 # host dir to copy result to
 DIST_DIR=dist-$VERSION
-IMAGE_NAME=rpi-grafana:$VERSION
+IMAGE_NAME=heziegl/rpi-grafana:$VERSION
 
 # build grafana runtime
 sudo docker build -t $IMAGE_NAME .
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 
 VERSION=2.6.0
 
-sudo docker run -p 3000:3000 -v $(pwd)/etc_grafana:/etc/grafana -v $(pwd)/var_lib_grafana:/var/lib/grafana -v $(pwd)/var_log_grafana:/var/log/grafana rpi-grafana:$VERSION
+sudo docker run -t -d -p 3000:3000 -v $(pwd)/etc_grafana:/etc/grafana -v $(pwd)/var_lib_grafana:/var/lib/grafana -v $(pwd)/var_log_grafana:/var/log/grafana heziegl/rpi-grafana:$VERSION
