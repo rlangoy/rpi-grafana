7a08e300044c14710d4c60416bf9e6df42b011c8
Heiko Ziegler <git@z5r.de>
2016-03-22 00:51:12 +0100

    added example of grafana.ini, cleanup

--- run.sh
+++ run.sh
@@ -3,4 +3,9 @@
 
 VERSION=2.6.0
 
-sudo docker run -t -d -p 3000:3000 -v $(pwd)/etc_grafana:/etc/grafana -v $(pwd)/var_lib_grafana:/var/lib/grafana -v $(pwd)/var_log_grafana:/var/log/grafana heziegl/rpi-grafana:$VERSION
+sudo docker run -t -d --name=grafana \
+                -p 3000:3000 \
+                -v $(pwd)/etc_grafana:/etc/grafana \
+                -v $(pwd)/var_lib_grafana:/var/lib/grafana \
+                -v $(pwd)/var_log_grafana:/var/log/grafana \
+                heziegl/rpi-grafana:$VERSION
