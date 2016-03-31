146ce8959bbd4afd71cbfd12985036376a867bfe
Heiko Ziegler <git@z5r.de>
2016-03-31 23:07:12 +0200

    push to docker hub

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@
 
 # grafana git tag to build
 VERSION=2.6.0
+DOCKERHUBUSER=heziegl
 
 # docker image to build
-IMAGE_NAME=heziegl/rpi-grafana:$VERSION
+IMAGE_NAME=$DOCKERHUBUSER/rpi-grafana:$VERSION
 
 # build grafana runtime
 sudo docker build -t $IMAGE_NAME .
+
+# push to docker hub
+sudo docker push $IMAGE_NAME
 
