62153d4f7bfe73384124392d384956b5e51f27c5
Rune Langøy <rune.langoy@gmail.com>
2016-09-16 19:34:21 +0200

    Create easyinstall.sh

---
+++ easyinstall.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo mkdir etc_grafana
+sudo mkdir grafana_data
+cd etc_grafana
+sudo wget https://raw.githubusercontent.com/heziegl/rpi-grafana/master/etc_grafana/grafana.ini
+cd ..
+sudo wget https://raw.githubusercontent.com/heziegl/rpi-grafana/master/run.sh
+sudo ./run.sh
