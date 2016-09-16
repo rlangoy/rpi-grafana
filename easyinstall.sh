4940fdbe53cb454712f7f7eb297d6e454889b6b2
Rune Langøy <rune.langoy@gmail.com>
2016-09-16 19:38:42 +0200

    Enabled execution for run.sh

--- easyinstall.sh
+++ easyinstall.sh
@@ -5,4 +5,5 @@ cd etc_grafana
 sudo wget https://raw.githubusercontent.com/heziegl/rpi-grafana/master/etc_grafana/grafana.ini
 cd ..
 sudo wget https://raw.githubusercontent.com/heziegl/rpi-grafana/master/run.sh
+sudo chmod +x ./run.sh
 sudo ./run.sh
